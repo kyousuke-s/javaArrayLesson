5df2f3797be632e1bb117161d93ee90c76b94c6b
kyou <kyousukesato8@gmail.com>
2020-11-29 01:00:25 +0900

    Q01~Q10回答

--- Q03/Main.java
+++ Q03/Main.java
@@ -2,8 +2,13 @@ import java.util.*;
 public class Main{
 	public static void main(String[] args){
 		//要素数3のdouble配列作成
+		double[] nums=new double[3];
 		for(int i=0;i<nums.length;i++){
 			//1.0から順番に詰める
+			if(i==nums.length-1){
+				break;
+			}
+			nums[i+1]=i+1.0;
 		}
 		System.out.println(Arrays.toString(nums));
 	}
